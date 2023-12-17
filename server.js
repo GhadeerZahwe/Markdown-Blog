d318f1d485082459694a43a45295074d01eb2bca
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-17 10:48:46 +0200

    fixes

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Article = require("./models/article");
-
 const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
 app.use("/articles", articleRouter);
-//create route
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
-  res.render("index", { articles: articles });
-});
 
 app.listen(5000);
