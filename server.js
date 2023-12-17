6a9cab20624d3e293de61fdf6693dc1b4b0d044c
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-17 09:38:45 +0200

    add articles

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use("/articles", articleRouter);
 //create route
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+    {
+      title: "Test Article 1",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+  ];
+  res.render("index", { articles: articles });
 });
 
 app.listen(5000);
