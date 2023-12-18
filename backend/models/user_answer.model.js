4a01f8348de4b22652383dd601ddeabddbe4f22e
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-18 03:36:30 +0200

    user survey

--- backend/models/user_answer.model.js
+++ backend/models/user_answer.model.js
@@ -6,6 +6,11 @@ const userAnswerSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+  surveyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Survey",
+    required: true,
+  },
   questionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Question",
@@ -16,12 +21,8 @@ const userAnswerSchema = new mongoose.Schema({
     ref: "QuestionAnswer",
     required: true,
   },
-  surveyId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Survey",
-    required: true,
-  },
 });
 
 const UserAnswer = mongoose.model("UserAnswer", userAnswerSchema);
+
 module.exports = UserAnswer;
