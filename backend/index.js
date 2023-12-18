94bb78e5da9c3591925c24b4ac61e521a0d48fc7
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-18 03:17:40 +0200

    question controller

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const { connectToMongoDB } = require("./configs/connection");
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
 require("dotenv").config();
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // auth route
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const surveyRoutes = require("./routes/survey.routes");
+app.use("/survey", surveyRoutes);
 app.listen(process.env.PORT, () => {
   console.log("Server listining on PORT: ", process.env.PORT);
   connectToMongoDB();
