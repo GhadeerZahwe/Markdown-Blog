4a226da3097c66f10f9d8d9f032f96f5534582ed
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-18 03:20:48 +0200

    add index routes

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,23 @@ app.use(cors({ origin: "http://localhost:3000" }));
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const { authMiddleware } = require("./middlewares/auth.middleware");
+//survey routes
 const surveyRoutes = require("./routes/survey.routes");
-app.use("/survey", surveyRoutes);
+app.use("/survey", authMiddleware, surveyRoutes);
+
+//question routes
+const questionRoutes = require("./routes/question.routes");
+app.use("/question", authMiddleware, questionRoutes);
+
+//question answer routes
+const questionAnswerRoutes = require("./routes/question_answer.routes");
+app.use("/question_answer", authMiddleware, questionAnswerRoutes);
+
+//user answer routes
+const userAnswerRoutes = require("./routes/user_answer.routes");
+app.use("/user_answer", authMiddleware, userAnswerRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log("Server listining on PORT: ", process.env.PORT);
   connectToMongoDB();
