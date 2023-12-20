03a595595d3d64fb25ace0f458a8591da348de8c
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-20 22:34:51 +0200

    predefined types

--- backend/models/type.model.js
+++ backend/models/type.model.js
@@ -10,4 +10,29 @@ const typeSchema = new mongoose.Schema({
 
 const Type = mongoose.model("Type", typeSchema);
 
+async function insertPredefinedTypes() {
+  try {
+    const count = await Type.countDocuments();
+
+    if (count === 0) {
+      await Type.insertMany([
+        {
+          _id: "6582d04ce29625e896bde17e",
+          name: "Radio",
+        },
+        {
+          _id: "65834f46e29625e896bde187",
+          name: "Checkbox",
+        },
+        {
+          _id: "65834f63e29625e896bde188",
+          name: "Input",
+        },
+      ]);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+insertPredefinedTypes();
 module.exports = Type;
