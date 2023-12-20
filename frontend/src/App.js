562aab9bb1b6e147bba135a198d9e209aad04e28
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-21 00:48:43 +0200

    add routing for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingUser from "./pages/User/LandingUser";
+import LandingAdmin from "./pages/Admin/LandingAdmin";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/landing" element={<LandingUser />} />
+          <Route path="/landing-admin" element={<LandingAdmin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
